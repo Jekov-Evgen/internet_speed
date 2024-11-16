d205da493ac712a744c9314cbc0dd34852104cf9
Jekov-Evgen <jena1245481te@gmail.com>
2024-11-16 15:58:24 +0200

    refactoring

--- Logic/speed.py
+++ Logic/speed.py
@@ -2,8 +2,9 @@ import speedtest
 
 def get_data_internet():
     st = speedtest.Speedtest()
+    st.get_best_server()
     download = st.download() / 1000000
     upload = st.upload() / 1000000
     ping = st.results.ping
     
-    return [download, upload, ping]
+    return [round(download, 2), round(upload, 2), round(ping, 2)]
